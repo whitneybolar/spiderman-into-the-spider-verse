6db41353d9ba0f7d64b9807352a5fe12f5258517
manikoth <colin@manikoth.com>
2019-01-29 20:55:57 -0800

    Version 1

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,21 @@
 // Everything OK?
 console.log('OK');
 
-// Your Awesome Scripts
+/* Lettering JS Plugin
+* Reference: https://github.com/davatron5000/Lettering.js/wiki
+*/
+
+// default
+// $('h1').lettering();
+
+// with Words options
+// $('h1').lettering('words');
+
+// with Lines options
+// $('h1').lettering('lines');
+
+/* Media Element Player Plugin
+* Reference: https://github.com/mediaelement/mediaelement/blob/master/docs/usage.md
+*/
+
+$('video').mediaelementplayer();
